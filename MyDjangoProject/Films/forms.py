22d66245011e2831977b7f92afc7e4d0c16805b4
PlatMatvey <matvey0910008@gmail.com>
2025-03-01 11:04:22 +0600

    My First Project

--- MyDjangoProject/Films/forms.py
+++ MyDjangoProject/Films/forms.py
@@ -4,11 +4,18 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 class CommentsForm(forms.ModelForm):
+    text = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Напишите текст'}))
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Имя пользователя'}))
+    day = forms.DateTimeField(widget=forms.TextInput(attrs={'class': 'form-control' 'datetimepicker'}))
     class Meta:
         model = Comments
-        fields = '__all__'
+        fields = ('text', 'username', 'day')
 
 class RegisterForm(UserCreationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Имя пользователя'}))
+    last_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Фамилия'}))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Пароль'}))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Повторите пароль'}))
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name', 'password1', 'password2')
+        fields = ('username', 'last_name', 'password1', 'password2')
